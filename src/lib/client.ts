e5799f5079d2da5be879589dd2c4a563c86859da
Davan <davanhs470@gmail.com>
2026-02-14 23:32:22 +0530

    fix: update client URL to match new app name

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,5 +1,4 @@
 import { treaty } from '@elysiajs/eden'
 import type { app } from '../app/api/[[...slugs]]/route'
 
-// .api to enter /api prefix
-export const client = treaty<typeof app>('next-js-chat-app-three.vercel.app').api
+export const client = treaty<typeof app>('termi-chat-app.vercel.app').api
